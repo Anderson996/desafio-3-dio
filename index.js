451d7ad3b4ce293a58dd3cd3cd7d268df7f37fdd
Anderson996 <99ldoand@gmail.com>
2024-10-15 20:06:32 -0300

    finalizado!

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-aaaaa
+//Classe
+class heroi {
+    constructor(nome , idade , tipo){
+    this.nome = nome
+    this.dade = idade
+    this.tipo = tipo
+    this.atanque
+    }
+
+    atacar(){
+        if(this.tipo === "mago"){
+            this.ataque = "magia"
+            console.log(`o ${this.tipo} atacou usando ${this.ataque}`)
+        }else if(this.tipo === "guerreiro"){
+            this.ataque = "espada"
+            console.log(`o ${this.tipo} atacou usando ${this.ataque}`)
+        }else if(this.tipo === "monge"){
+            this.ataque = "artes marciais"
+            console.log(`o ${this.tipo} atacou usando ${this.ataque}`)
+        }else if(this.tipo === "ninja"){
+            this.ataque = "shuriken"
+            console.log(`o ${this.tipo} atacou usando ${this.ataque}`)
+        }
+    }
+}
+
+let ataqueHeroi = new heroi("Felipão" , 25 , "ninja")
+
+ataqueHeroi.atacar()
